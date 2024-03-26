92d31d9f8b84dd0c3b06f07637c138476b3b4601
Zane <michael.zane@capgemini.com>
2024-03-26 14:36:59 +0100

    .

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
                                 responses.push(convertStringToDate(cell.value));
                             } else {
                                 var s = cell.value + "";
-                                responses.push(s.replaceAll(";","&lt;br&gt;"));
+                                responses.push(s);
                             }
                         }
                         else
