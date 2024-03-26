7ac8dbfdf73eae85b3f9bec4651847edfa16d91d
Zane <michael.zane@capgemini.com>
2024-03-26 10:52:21 +0100

    .

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ app.get('/download', function (req, res) {
                         questions: questions,
                         responses: responses
                     }
-                    fileName = "IdentikitFinanziario_" + responses[4].trim().replaceAll(" ", "_") + ".pdf";
+                    fileName = "IdentikitFinanziario_" + responses[6].trim().replaceAll(" ", "_") + ".pdf";
                     filesName.push(fileName);
                     document.path = dirname + fileName;
                     pdf.create(document, {
